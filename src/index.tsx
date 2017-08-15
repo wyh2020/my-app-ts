68622a6c7a1f7b378721ade32d7107675deb7237
of1410 <wangyonghua@qianmi.com>
2017-08-15 19:29:21 +0800

    使用Redux编写Hello组件、使用Jest编写测试

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+// import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello/>
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
